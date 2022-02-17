b627f903f04528b74800dbc8fb37e5ed8a050b9c
Narrator0116 <3175635937@qq.com>
2022-02-17 13:36:59 +0800

    初始化仓库

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+  const getMock = () => {
+      var loginMode = 'msdk';//游戏内默认msdk(wx|qq|msdk)
+      var url = '/api/msdk/proxy/query_common_credit';
+      this.$axios.get(url)
+        .then(response => {
+          
+        })
+        .catch(error => {
+          
+        })
+    
+  }
+</script>
+
+<template>
+ <button @click="getMock"></button>
+</template>
+
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './api/index'//此部分引入的是我们所编写的mockjs文档
+createApp(App).mount('#app')
